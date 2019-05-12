b7fb5f9f2dfeb47b8c24fd9ea3f98193e67f35b3
tom_glb <goodMorning_glb@atguign.com>
2019-05-12 09:55:12 +0800

    222

--- src/main/java/com/hr/git/TestGit.java
+++ src/main/java/com/hr/git/TestGit.java
@@ -3,7 +3,7 @@ package com.hr.git;
 public class TestGit {
 	public static void main(String[] args) {
 		System.out.println("Test...");
-		System.out.println("bbb...");
+		System.out.println("ccc...");
 		System.out.println("cc...");
 	}
 }
